765ea14755382c7e1d428ce46c7637f282df9b97
Sebastian Wozniak <sebastianwozniak91@gmail.com>
2019-06-18 19:17:04 +0200

    created database relation, created superuser

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    rate = models.CharField(max_length=5, null=True, blank=True)
+    movie_id = models.IntegerField(null=True, blank=True)
+    date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.rate} - {self.movie_id}"
+
+
+class Tag(models.Model):
+    user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=120, null=True, blank=True)
+    genres = models.ManyToManyField(Genre, null=True, blank=True)
+    ratings = models.ManyToManyField(Rating, null=True, blank=True)
+    year = models.CharField(max_length=5, null=True, blank=True)
+    link = models.CharField(max_length=120, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
