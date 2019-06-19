cd3f18337e940eeb4aa7398508e520a7c6f79d03
Sebastian Wozniak <sebastianwozniak91@gmail.com>
2019-06-19 11:22:45 +0200

    created test for movie model

--- movie/tests/test_models.py
+++ movie/tests/test_models.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 from django.contrib.auth.models import User
 
-from movie.models import Genre, Rating, Tag
+from movie.models import Genre, Rating, Tag, Movie
 
 
 class GenreTest(TestCase):
@@ -63,6 +63,48 @@ class TagTest(TestCase):
     def tearDown(self):
         print("Remove tag objects")
         self.tag.delete()
+        self.user.delete()
 
     def test_tag_instance(self):
         self.assertTrue(isinstance(self.tag, Tag))
+
+
+class MovieTest(TestCase):
+
+    def setUp(self):
+        print("Setup objects for tag model")
+        self.genre = Genre.objects.create(name='Comedy')
+        self.another_genre = Genre.objects.create(name='Crime')
+        self.user = User.objects.create(
+            username='user_test',
+            email='user@test.com'
+        )
+        self.date = datetime.datetime.now()
+        self.rating = Rating.objects.create(
+            user=self.user,
+            rate='5.0',
+            movie_id='1',
+            date=self.date
+        )
+        self.tag = Tag.objects.create(
+            user=self.user,
+            name='great movie'
+        )
+        self.movie = Movie.objects.create(
+            title='King Lion',
+            year='1999',
+            link='http://imdb.com/tt/12312312'
+        )
+        self.movie.genres.add(self.genre)
+        self.movie.genres.add(self.another_genre)
+        self.movie.ratings.add(self.rating)
+
+    def tearDown(self):
+        print("Remove movie objects")
+        self.movie.delete()
+        self.user.delete()
+        self.genre.delete()
+        self.another_genre.delete()
+
+    def test_movie_instance(self):
+        self.assertTrue(isinstance(self.movie, Movie))
